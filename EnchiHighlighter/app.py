ce7836a14e88546df6dc97018785a207a513f7d5
ShinySylvie <jordynmbrown@gmail.com>
2024-10-12 19:31:28 -0400

    mainly added suggestions of what to add

--- EnchiHighlighter/app.py
+++ EnchiHighlighter/app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 Keywords = {
     #operators array within a dictionary
     "operators": ["+", "-", "*", "/", "="]
-
+    #loops (for, while, do, ect)
+    #conditional statements (if, else, elif, ect)
+    #keywords (def, import, return ect)
+    #basic functions (len, range, sort, ect)
+    #logic and boolean? (and, or, True, false, ect)    
 
 }
