88cdd31807ec5f8612e0fbc9a20a335ad9a22454
Meenol <kaylaknoxclark1234@gmail.com>
2024-10-12 20:09:56 -0400

    dictionary added

--- EnchiHighlighter/app.py
+++ EnchiHighlighter/app.py
@@ -2,13 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-Keywords = {
-    #operators array within a dictionary
-    "operators": ["+", "-", "*", "/", "="]
-    #loops (for, while, do, ect)
-    #conditional statements (if, else, elif, ect)
-    #keywords (def, import, return ect)
-    #basic functions (len, range, sort, ect)
-    #logic and boolean? (and, or, True, false, ect)    
-
+SyntaxBank = {
+    "operators": ["+", "-", "*", "/", "="],
+    "keywords" : ["for","while","if", "else","elif", "in"],
+    "litfun" : ["int", "print", "float", "range", "len", "string", "bool","dict",
+                "str","list","set", "tuple", "bytes","sum", "round", "sorted","chr",
+                "bin", "dir","vars","type","0","1","2","3","4","5","6","7","8","9"]
+  
 }
