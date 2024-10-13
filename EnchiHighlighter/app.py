564e9296929db56e5525fe18300d5a32415b4400
ShinySylvie <jordynmbrown@gmail.com>
2024-10-12 20:26:41 -0400

    Created the CSS for dictionaries and created reference for app.py

--- EnchiHighlighter/app.py
+++ EnchiHighlighter/app.py
@@ -10,3 +10,10 @@ SyntaxBank = {
                 "bin", "dir","vars","type","0","1","2","3","4","5","6","7","8","9"]
   
 }
+
+@app.route('/syntax')
+def get_Bank():
+    return jsonify(SyntaxBank)
+
+if __name__ == '__main__':
+    app.run(debug=True)
