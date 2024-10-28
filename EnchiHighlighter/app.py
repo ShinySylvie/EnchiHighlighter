43324988cd6ad58e516f8c51a234b3682d324798
Meenol <kaylaknoxclark1234@gmail.com>
2024-10-27 21:33:37 -0400

    WorkChecker + Revised hmtl file

--- EnchiHighlighter/app.py
+++ EnchiHighlighter/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask,request,render_template,jsonify
 
 app = Flask(__name__)
 
@@ -7,13 +7,71 @@ SyntaxBank = {
     "keywords" : ["for","while","if", "else","elif", "in"],
     "litfun" : ["int", "print", "float", "range", "len", "string", "bool","dict",
                 "str","list","set", "tuple", "bytes","sum", "round", "sorted","chr",
-                "bin", "dir","vars","type","0","1","2","3","4","5","6","7","8","9"]
+                "bin", "dir","vars","type"]
   
 }
+ #Checks if 'word' is number
+def is_number(word):
+    try:
+        float(word)
+        return True
+    except ValueError:
+        return False
 
-@app.route('/syntax')
+#Add 'word' to SyntaxBank if it's not already there
+def add_to_litfun(word):
+    if word not in SyntaxBank['litfun']:
+        SyntaxBank['litfun'].append(word)
+
+#                               *** _____Routing Section____***
+
+@app.route('/')
+def index():
+    return render_template('SyntaxHighlighter.html')
+    
+
+@app.route('/syntax', methods=['GET'])
 def get_Bank():
     return jsonify(SyntaxBank)
 
+@app.route('/process_word', methods=['POST','GET'])
+def process_word():
+    data = request.json  
+    word = data.get('word')
+    
+    #Word Checker
+    if word:
+        found_in_category = None
+        
+        if is_number(word):
+            found_in_category = "litfun"
+            add_to_litfun(word)
+            
+        for category, words_list in SyntaxBank.items():
+            if word in words_list:
+                found_in_category = category
+                break
+
+
+        if found_in_category:
+            return jsonify({
+                'message': f"Found word: '{word}' in category: {found_in_category}",
+                'word': word,
+                'is_found': True
+        
+            }), 200
+        else:
+            return jsonify({
+                'message': f"Word '{word}' not found in any category",
+                'word': word,
+                'is_found': False
+                }),200   
+    else:
+       
+        return jsonify({'error': 'Not keyword'}), 400
+
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
